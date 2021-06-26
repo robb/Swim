a270fed426f670be40bf6e226dd3092a34a107ea
Robert Böhnke <robb@robb.is>
2021-06-26 18:12:56 +0200

    Add test for custom attribute value

--- Tests/SwimTests/SwimTests.swift
+++ Tests/SwimTests/SwimTests.swift
@@ -27,4 +27,36 @@ final class SwimTests: XCTestCase {
         ], nil)
         XCTAssertEqual(n.rendered, "\n<a x=\"&quot;\"/>")
     }
+
+    func testCustomAttributeValue() {
+        let color = Color(red: 1, green: 0.5, blue: 0.5)
+
+        let n: Node = Node.element("custom", [
+            "color": color
+        ], nil)
+
+        XCTAssertEqual(n.rendered, "\n<custom color=\"1.0,0.5,0.5\"/>")
+
+        if case let .element(_, attributes, _) = n {
+            XCTAssertEqual(attributes["color"]?.base as? Color, color)
+        } else {
+            XCTFail()
+        }
+    }
+}
+
+struct Color: Hashable, TextOutputStreamable {
+    var red: Float
+
+    var green: Float
+
+    var blue: Float
+
+    func write<Target>(to target: inout Target) where Target : TextOutputStream {
+        target.write(String(red))
+        target.write(",")
+        target.write(String(green))
+        target.write(",")
+        target.write(String(blue))
+    }
 }
