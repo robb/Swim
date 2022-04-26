b88248638bde427425b9995dbb12df51e563f160
Robert Böhnke <robb@robb.is>
2022-04-26 14:54:26 +0200

    Make AttributeKey a struct

--- Sources/Swim/AttributeKey.swift
+++ Sources/Swim/AttributeKey.swift
@@ -1,30 +1,64 @@
 import Foundation
 
-public enum AttributeKey: Hashable, Comparable {
-    case raw(String)
+public struct AttributeKey: Hashable {
+    internal enum Storage: Hashable, Comparable {
+        case raw(String)
+        case scoped(String, String)
+    }
 
-    case scoped(String, String)
+    var storage: Storage
+
+    public init(_ raw: String) {
+        self.storage = .raw(raw)
+    }
+
+    fileprivate init(storage: Storage) {
+        self.storage = storage
+    }
+
+    public var scope: String? {
+        if case let .scoped(scope, _) = storage {
+            return scope
+        }
+
+        return nil
+    }
+
+    public var name: AnyHashable {
+        switch storage {
+        case let .raw(string):
+            return string
+        case let .scoped(_, string):
+            return string
+        }
+    }
 
     public static func data(_ value: String) -> Self {
-        .scoped("data", value)
+        Self(storage: .scoped("data", value))
+    }
+}
+
+extension AttributeKey: Comparable {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs.storage < rhs.storage
     }
 }
 
 public extension AttributeKey {
     static func ~= (lhs: Self, rhs: String) -> Bool {
-        lhs == .raw(rhs)
+        lhs.storage == .raw(rhs)
     }
 }
 
 extension AttributeKey: ExpressibleByStringLiteral {
     public init(stringLiteral value: StringLiteralType) {
-        self = .raw(value)
+        self.storage = .raw(value)
     }
 }
 
 extension AttributeKey: TextOutputStreamable {
     public func write<Target>(to target: inout Target) where Target : TextOutputStream {
-        switch self {
+        switch storage {
         case let .raw(key):
             key.write(to: &target)
         case let .scoped(namespace, key):
