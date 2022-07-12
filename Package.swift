d3dde8cf781507ada3b7549996bea94f8d57181f
Robert Böhnke <robb@robb.is>
2022-07-12 14:04:12 +0200

    Make platforms explicit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "HTML",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
